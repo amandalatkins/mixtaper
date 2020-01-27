1583f5559a9655e6c636fac06dc1d7fbea285d09
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-01-27 13:19:52 -0800

    otheruserplaylist.js update. beginnings of index.js assembly

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    var loginInput = $("#login-input");
+    var passwordInput = $("#password-input");
+
+    $(document).on("click", "login-button", handleLoginPress);
+    $(document).on("click", "create-account-button", handleCreateAccountPress);
+
+//login button
+//redirects to authentication then to userPlaylists
+function handleLoginPress(e){
+    if ()
+}
+
+//create account button
+//saves login information to user db 
+//then redirects to userPlaylists
+function handleCreateAccountPress(e){
+
+}
+});
+
