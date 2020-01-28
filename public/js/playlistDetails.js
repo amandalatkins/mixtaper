fdcdfb4960b9c56b159751a1efb717e37797d6b6
Amanda Atkins <amanda@digitalrainstorm.com>
2020-01-27 18:49:28 -0800

    jQuery to pull playlist and songs and append to page

---
+++ public/js/playlistDetails.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    var nameContainer = $('#playlistName');
+    var playlistSongs = $('#playlistSongs');
+
+    var playlistId = location.pathname.replace('/playlists/','');
+    
+    $.get('/api/playlists/'+playlistId).then(data => {
+        nameContainer.text(data.name);
+        data.Songs.forEach(song => {
+            var songRow = $('<tr>');
+            songRow.data(song);
+            songRow.append("<td><strong>"+song.title+"</strong></td>");
+            songRow.append("<td>"+song.artist+"</td>");
+            songRow.append("<td>"+song.genre+"</td>");
+            songRow.append(`<td><a href="${song.link}">Play</a> | <a href="#" class="remove-song" data-id="${song.id}">Remove From Playlist</a>`);
+
+            playlistSongs.append(songRow);
+        });
+    });
+
+});
