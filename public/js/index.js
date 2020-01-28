eb20a4799ca360c94a588637467e287b720573ad
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-01-28 14:25:04 -0800

    userPlaylist.js assembly and debug

--- public/js/index.js
+++ public/js/index.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
     var loginInput = $("#login-input");
     var passwordInput = $("#password-input");
 
-    $(document).on("click", "login-button", handleLoginPress);
-    $(document).on("click", "register-button", handleCreateAccountPress);
+    $(document).on("click", ".login-button", handleLoginPress);
+    $(document).on("click", ".register-button", handleCreateAccountPress);
 
 //login button
 //redirects to authentication then to userPlaylists
@@ -16,8 +16,8 @@ function handleLoginPress(e){
     }
     $.ajax('/api/users/login', {
         data: {
-          username: $('#login-input .username').val().trim(),
-          password: $('#password-input .password').val().trim()
+          username: $('.login-input').val().trim(),
+          password: $('.password-input').val().trim()
         },
         type: "POST"
     }).then(function(response) {
@@ -30,14 +30,15 @@ function handleLoginPress(e){
 
 function handleCreateAccountPress(e){
     e.preventDefault();
+    console.log("cool")
     if (!loginInput || !passwordInput) {
         alert("Please enter your username and password");
         return;
     }
     $.ajax('/api/users/register', {
         data: {
-          username: $('#login-input .username').val().trim(),
-          password: $('#password-input .password').val().trim()
+          username: $('.login-input').val().trim(),
+          password: $('.password-input').val().trim()
         },
         type: "POST"
     }).then(function(response) {
--- public/js/userPlaylist.js
+++ public/js/userPlaylist.js
@@ -1,27 +1,36 @@
 $(document).ready(function(){
 // Getting references to the playlist input
     var playlistVar = $("#playlist-name");
+    var playlistRow = $(".playlist-name")
     var userVar = $("#username-list");
 
 // Adding event listeners to the form to create a new object, and the button to delete
 // a playlist
-$(document).on("submit", "#add-playlist", handleAddPlaylistPress);
-$(document).on("click", "#delete-playlist", handleDeletePlaylistPress);
+$(document).on("click", "#add-playlist", handleAddPlaylistPress);
+$(document).on("click", ".delete-playlist", handleDeletePlaylistPress);
 
+//get user_id global variable
+var currentUser;
+$.get('/api/user_data', function(CU) {
+currentUser = CU;
 // Getting the initial list of playlists
 getPlaylists();
+});
+
 
 // A function to handle what happens when the form is submitted to create a new playlist
 function handleAddPlaylistPress(event){
+  console.log("buttonclicktest")
     event.preventDefault();
 // Don't do anything if the name fields hasn't been filled out
-if (!playlistInput.val().trim().trim()) {
+if (!playlistVar.val().trim().trim()) {
     return;
 }
 
 
 // Calling the insertPlaylist function and passing in the value of the name input
 insertPlaylist({
+    userId: currentUser.id,
     name: playlistVar
       .val()
       .trim()
@@ -32,32 +41,33 @@ insertPlaylist({
 // A function for creating an pl. Calls getPlaylists upon completion
 function insertPlaylist(playlistData) {
     $.post("/api/playlists", playlistData)
-      .then(getPlaylists);
+    .then(function(){
+      location.reload();
+    });
   }
 
 // Function for creating a new list row for PLs
 function createPlaylistRow(playlistData) {
+  console.log("thing")
     var newPl = $("<tr>");
     newPl.data("playlist", playlistData);
     newPl.append("<td>" + playlistData.name + "</td>");
-    if (playlistData.Posts) {
-      newPl.append("<td> " + playlistData.Posts.length + "</td>");
-    } else {
-      newPl.append("<td>0</td>");
-    }
-    newPl.append("<td><a href='/playlistDetails?playlist_id=" + playlistData.id + "'>Go to Playlist</a></td>");
-    newPl.append("<td><a href='/cms?playlist_id=" + playlistData.id + "'>Create Playlist</a></td>");
-    newPl.append("<td><a style='cursor:pointer;color:red' class='delete-playlist'>Delete Playlist</a></td>");
+    newPl.append("<td><a href='/playlists/" + playlistData.id + "'>Go to Playlist</a></td>");
+    newPl.append("<td><a style='cursor:pointer;color:red' class='delete-playlist' data-id= '"+ playlistData.id + "'>Delete Playlist</a></td>");
     return newPl;
   }
 
 // Function for retrieving playlists and getting them ready to be rendered to the page
 function getPlaylists() {
-    $.get("/api/userplaylists", function(data) {
+
+    var user_id = currentUser.id;
+
+    $.get("/api/users/"+user_id, function(data) {
       var rowsToAdd = [];
-      for (var i = 0; i < data.length; i++) {
-        rowsToAdd.push(createPlaylistRow(data[i]));
+      for (var i = 0; i < data.Playlists.length; i++) {
+        rowsToAdd.push(createPlaylistRow(data.Playlists[i]));
       }
+      
       renderPlaylistList(rowsToAdd);
       playlistVar.val("");
     });
@@ -68,35 +78,37 @@ function renderPlaylistList(rows) {
     // playlistVar.children(".alert").remove();
     if (rows.length) {
       console.log(rows);
-      playlistVar.prepend(rows);
+      playlistRow.prepend(rows);
     }
     }
 
 // Function for handling what happens when the delete button is pressed
 function handleDeletePlaylistPress() {
-    var listItemData = $(this).parent("td").parent("tr").data("playlists");
-    var id = listItemData.id;
+    var id = $(this).data("id");
     $.ajax({
       method: "DELETE",
       url: "/api/playlists/" + id
     })
-      .then(getPlaylists);
+      .then(function(){
+        location.reload();
+      });
+
   }
 //__________________________________________________________________________________________________________________
-  function createUserRow(userData) {
-    var userList = $("<tr>");
-    userList.data("user", userData);
-    userList.append("<td>" + userData.name + "</td>");
-    if (playlistData.Posts) {
-      userList.append("<td> " + playlistData.Posts.length + "</td>");
-    } else {
-      userList.append("<td>0</td>");
-    }
-    userList.append("<td><a href='/playlistDetails?playlist_id=" + userData.id + "'>Go to Playlist</a></td>");
-    userList.append("<td><a href='/cms?playlist_id=" + usertData.id + "'>Create Playlist</a></td>");
-    userList.append("<td><a style='cursor:pointer;color:red' class='delete-playlist'>Delete Playlist</a></td>");
-    return userList;
-  }
+  // function createUserRow(userData) {
+  //   var userList = $("<tr>");
+  //   userList.data("user", userData);
+  //   userList.append("<td>" + userData.name + "</td>");
+  //   if (playlistData.Posts) {
+  //     userList.append("<td> " + playlistData.Posts.length + "</td>");
+  //   } else {
+  //     userList.append("<td>0</td>");
+  //   }
+  //   userList.append("<td><a href='/playlistDetails?playlist_id=" + userData.id + "'>Go to Playlist</a></td>");
+  //   userList.append("<td><a href='/cms?playlist_id=" + usertData.id + "'>Create Playlist</a></td>");
+  //   userList.append("<td><a style='cursor:pointer;color:red' class='delete-playlist'>Delete Playlist</a></td>");
+  //   return userList;
+  // }
   
 
 });
